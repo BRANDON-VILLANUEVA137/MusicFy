14b4127a970541c73d798bf9baa3929a1110fbd9
Brandon Villanueva <brandonvillanueva137@gmail.com>
2025-05-22 18:18:59 -0500

    error en el fecth version 3

--- models/cancionesModel.js
+++ models/cancionesModel.js
@@ -1,13 +1,11 @@
 import db from '../config/db.js';
 
-export const getAllSongs = () => {
-  return new Promise((resolve, reject) => {
-    const query = 'SELECT id, titulo, artista, youtube_url FROM canciones ORDER BY created_at DESC';
-    db.query(query, (err, results) => {
-      if (err) {
-        return reject(err);
-      }
-      resolve(results);
-    });
-  });
+export const getAllSongs = async () => {
+  const query = 'SELECT id, titulo, artista, youtube_url FROM canciones ORDER BY created_at DESC';
+  try {
+    const [results] = await db.query(query);
+    return results;
+  } catch (error) {
+    throw error;
+  }
 };
