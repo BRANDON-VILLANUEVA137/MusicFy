e3cc1c30a29a21d83f00391b063a149867796407
Brandon Villanueva <brandonvillanueva137@gmail.com>
2025-05-22 18:16:01 -0500

    error en el fecth version 2

--- Routes/cancionesRoutes.js
+++ Routes/cancionesRoutes.js
@@ -2,6 +2,6 @@ import express from 'express';
 const router = express.Router();
 import { getSongs } from '../controllers/cancionesController.js';
 
-router.get('/api/canciones', getSongs);
+router.get('/canciones', getSongs);
 
 export default router;
